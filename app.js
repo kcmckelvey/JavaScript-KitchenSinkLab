2dab2ce163a065139ac5510189fcfeabbdf97149
Kevin McKelvey <kevinmckelvey920@gmail.com>
2018-08-21 17:28:39 -0500

    updates

---
+++ app.js
@@ -0,0 +1,42 @@
+let firstName = 'Kevin'
+
+console.log(firstName);
+
+const numberOfStates = 50
+
+console.log(numberOfStates);
+
+let sum = 5 + 4;
+
+console.log(sum);
+
+
+
+function sayHello() {
+    return alert('Hello World');
+}
+
+function checkAge(name, age) {
+    if (age < 21) {
+        return alert('Sorry ' + name + '. You are not old enough to view this site')
+    } else {
+
+    }
+} 
+checkAge('Charles', 21)
+checkAge('Abby', 27)
+checkAge('James', 18)
+checkAge('John', 17)
+
+let veggies = ['lettuce', 'broccoli', 'asparagus']
+
+for(let va = 0; va < veggies.length; va++) {
+    console.log(veggies[va])
+}
+
+
+
+function getLength(pizza){
+    return string.length
+}
+console.log(getLength)
